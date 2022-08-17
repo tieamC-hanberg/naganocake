1f22dd26e70dca023419e807950d07548182dbe1
EC2 Default User <ec2-user@ip-172-31-47-108.ap-northeast-1.compute.internal>
2022-08-17 14:21:12 +0000

    更新

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,12 +1,6 @@
 class Admin::HomesController < ApplicationController
   def top
-    @order_details = Order_details.all
-  end
-  
-  private
-    
-  def order_details_params
-    params.require(:order).permit(:postcode, :address, :total_payment, :order_status, :item_id)
+    @order_details = OrderDetail.all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,13 @@ Rails.application.routes.draw do
   get "homes/about"=>"homes#about"
   # get "items/index"=>"items#index"
   resources :items, only: [:index, :show]
+  resources :deliveries, only: [:index, :create, :edit]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :admin do
+  get "homes/top" => "homes#top"
   resources :items, only: [:index, :new, :create, :show, :edit]
   # resource :items, only: [:new, :create]
+  resources :genres, only: [:index, :create, :edit, :update]
   end
 end
