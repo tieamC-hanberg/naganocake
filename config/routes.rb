b7d11e5b3091c6adca808f04f3b3e71ee4239382
EC2 Default User <ec2-user@ip-172-31-20-112.ap-northeast-1.compute.internal>
2022-08-17 01:41:22 +0000

    フランスを感じる

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  
+  #get 'homes/top'
+  root to: 'public/homes#top'
+  namespace :public do
+  get "homes/about"=>"homes#about"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
