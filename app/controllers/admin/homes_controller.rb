a93edd8b20712c475a7803889e27d1a00449f239
EC2 Default User <ec2-user@ip-172-31-47-108.ap-northeast-1.compute.internal>
2022-08-17 07:26:36 +0000

    更新

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,8 +1,12 @@
 class Admin::HomesController < ApplicationController
   def top
+    @order_details = Order_details.all
   end
   
-  def about
+  private
+    
+  def order_details_params
+    params.require(:order).permit(:postcode, :address, :total_payment, :order_status, :item_id)
   end
-  
+
 end
--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,2 +1,20 @@
 class Public::DeliveriesController < ApplicationController
+  def index
+    @deliverie = Deliverie.find(params[:id])
+    @deliveries = Deliverie.all
+  end
+  
+  def create
+    @deliverie = Deliverie.find(params[:id])
+    @deliveries = Deliverie.all
+  end
+  
+  def edit
+  end
+  
+  private
+
+  def deliverie_params
+    params.require(:deliverie).permit(:postcode, :address, :name)
+  end
 end
