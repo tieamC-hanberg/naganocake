e928192d82f557cc6542b8238a4604ae92d67207
EC2 Default User <ec2-user@ip-172-31-47-108.ap-northeast-1.compute.internal>
2022-08-20 06:39:45 +0000

    更新

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,25 @@
 class Public::OrdersController < ApplicationController
+  def complete
+  end
+  
+  def comfilm
+    @order = Order.new(order_params)
+    binding.pry
+  end
+  
+  def index
+  end
+  
+  def new
+  end
+  
+  def show
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:payment_method)
+  end
+  
 end
