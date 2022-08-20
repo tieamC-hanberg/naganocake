fa4f5c6dae70f8f534618afd91ed3cc608bf0fc8
EC2 Default User <ec2-user@ip-172-31-20-112.ap-northeast-1.compute.internal>
2022-08-20 05:14:09 +0000

    public

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,2 +1,36 @@
-class Public::SessionsController < ApplicationController
+# frozen_string_literal: true
+
+class Public::SessionsController < Devise::SessionsController
+  
+  def after_sign_in_path_for(resource)
+     #public_homes_about_path
+     public_customer_path(current_customer)
+  end
+  
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,26 @@
 Rails.application.routes.draw do
-  #get 'homes/top'
+  devise_for :admins, controllers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :customers, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+ 
+  # ,skip: [:passwords], controllers: {
+  # registrations: "customer/registrations",
+  # sessions: 'customer/sessions'
+  # }
+  
   root to: 'public/homes#top'
+  #root to: 'admin/homes#top'
+  
   namespace :public do
+  #scope module: :customer do
   get "homes/about"=>"homes#about"
+  get "/unsubscribe"=>"customers#unsubscribe"
+  patch "/withdrawal"=>"customers#withdrawal"
+  resources :customers, only: [ :show, :edit, :update]
   resources :items, only: [:index, :show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
