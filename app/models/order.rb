ce5abf03c55bcc896b4169d947f5c41885996229
EC2 Default User <ec2-user@ip-172-31-19-196.us-west-1.compute.internal>
2022-08-22 07:34:04 +0000

    更新

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,11 @@
 class Order < ApplicationRecord
-  belongs_to :customers
+  belongs_to :customer
   has_many :order_details
 
   enum payment_method: { credit_card: 0, transfer: 1 }
 
   def address_display
-  '〒' + postal_code + ' ' + address + ' ' + name
+  '〒' + postcode + ' ' + address + ' ' + name
   end
+  
 end
