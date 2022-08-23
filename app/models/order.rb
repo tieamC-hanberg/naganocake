49f436bd9b3fa3ea22265114a67948ab003472e5
EC2 Default User <ec2-user@ip-172-31-19-196.us-west-1.compute.internal>
2022-08-23 06:18:41 +0000

    up

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,14 +4,9 @@ class Order < ApplicationRecord
   has_many :order_details
 
   enum payment_method: { credit_card: 0, transfer: 1 }
-  enum delivery_method: { customer_address: 0, delivery_address: 1, new_address:2 }
 
   def address_display
   '〒' + postcode + ' ' + address + ' ' + name
   end
   
-  def subtotal
-    amount*item.with_tax_price.to_i
-  end
-
 end
