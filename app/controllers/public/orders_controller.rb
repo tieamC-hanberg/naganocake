aa2808573dce45f81f22eb548277e2d5b6a385fb
EC2 Default User <ec2-user@ip-172-31-19-196.us-west-1.compute.internal>
2022-08-24 05:57:29 +0000

    tyuumonn

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,30 +1,29 @@
 class Public::OrdersController < ApplicationController
   def complete
-  
+
   end
 
- def confirm
-    
+  def confirm
     @total = 0
     @order = Order.new(order_params)
     #@delivery = Delivery.find(params[:order][:delivery_id][:payment_method])
     # @order.payment_method = params[:order][:payment_method]
     @cart_items = CartItem.all
-    if params[:order][:delivery_id] == "0"
+    if params[:order][:delivery_number] == "1"
       @order.postcode = current_customer.postcode
       @order.address = current_customer.address
-      @order.name = current_customer.first_name + current_customer.last_name
-    elsif params[:order][:delivery_id] == "1"
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:delivery_number] == "2"
       @order.postcode = Delivery.find(params[:order][:delivery_id]).postcode
       @order.address = Delivery.find(params[:order][:delivery_id]).address
       @order.name = Delivery.find(params[:order][:delivery_id]).name
-    elsif params[:order][:delivery_id] == "2"
+    elsif params[:order][:delivery_number] == "3"
       @order.postcode = Order.new(order_params).postcode
       @order.address = Order.new(order_params).address
       @order.name = Order.new(order_params).name
     end
   end
-   
+
 
   def index
   end
@@ -37,7 +36,12 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
- 
+    @order = current_customer.orders.new(order_params)
+    @order.save
+    @cart_items = current_customer.cart_items.all
+    @cart_items.destroy_all
+
+    redirect_to public_orders_complete_path
   end
 
   def show
@@ -46,7 +50,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment_method, :delivery_id, :postcode, :address, :name, :first_name, :last_name, :amount, :item_id)
+    params.require(:order).permit(:payment_method, :postcode, :address, :name, :first_name, :last_name, :amount, :item_id, :freight, :total_payment)
   end
 
   def cart_item_params
