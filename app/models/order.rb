46ccbdbbc6e59fae40557e07c4d7ea5f6d9034bb
EC2 Default User <ec2-user@ip-172-31-19-196.us-west-1.compute.internal>
2022-08-24 08:54:54 +0000

    upp

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :customer
-  has_many :order_detail
+  has_many :order_details
 
-  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum payment: { credit_card: 0, transfer: 1 }
 
   enum delivery_method: { customer_address: 0, delivery_address: 1, new_address:2 }
+
+  enum order_status: {waiting: 0, paid_up: 1, production: 2, preparing: 3, shipped: 4}
 
   def address_display
   '〒' + postcode + ' ' + address + ' ' + name
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,4 @@
 class OrderDetail < ApplicationRecord
-  belongs_to :order_params
+  belongs_to :order
   belongs_to :item
 end
