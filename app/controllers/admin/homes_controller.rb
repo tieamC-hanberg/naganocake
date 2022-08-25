9cdcc533ad8d5a89b0da8e5cb38ee4ce2244a86f
EC2 Default User <ec2-user@ip-172-31-19-196.us-west-1.compute.internal>
2022-08-25 05:00:16 +0000

    asd

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,6 @@
 class Admin::HomesController < ApplicationController
   def top
-    @orders = Orders.all
+    @orders = Order.all
   end
 
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -50,7 +50,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment_method, :postcode, :address, :name, :first_name, :last_name, :amount, :item_id, :freight, :total_payment)
+    params.require(:order).permit(:payment_method, :postcode, :address, :name, :first_name, :last_name, :amount, :item_id, :freight, :total_payment, :order_status)
   end
 
   def cart_item_params
