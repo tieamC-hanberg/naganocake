0acbe8d62f9027a30e430b431e71d6ad454a9d94
EC2 Default User <ec2-user@ip-172-31-19-196.us-west-1.compute.internal>
2022-08-25 07:13:05 +0000

    qq

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,13 +1,13 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-    @order_dateil = OrderDetail.find(params[:id])
-    @order.update(order_params)
-    redirect_to admin_homes_top_path(@order)
+    @order_detail = OrderDetail.find(params[:id])
+    @order.update(order_detail_params)
+    redirect_to admin_order_path(order_detail)
   end
 
   private
   def order_dateil_params
-    params.require(:order_dateil).permit(:make_status)
+    params.require(:order_dateil).permit(:make_status, :order_id, :tax_price, :amount, :item)
   end
 
 end
